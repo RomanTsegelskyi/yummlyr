388240bdba7ab4290e900f6a75827c7efe634e4c
Roman Tsegelskyi <roman.tsegelskyi@gmail.com>
2015-09-04 12:59:59 -0700

    Added updater.sh

---
+++ updater.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+GH_REPO="@github.com/RomanTsegelskyi/yummlyr.git"
+FULL_REPO="https://$GH_TOKEN$GH_REPO"
+
+mkdir out
+cd out
+git init
+git remote add origin $FULL_REPO
+git fetch
+git config user.name "rapporter-travis"
+git config user.email "travis"
+
+# handle master
+git checkout master
+CHANGED_FILES=`git show --stat $TRAVIS_COMMIT`
+# check if vignettes were updated
+if [[ $CHANGED_FILES =~ .*vignettes.*\.Rmd.* ]]
+then
+  R -e 'devtools::install_github("rstudio/rmarkdown"); devtools::build_vignettes()'
+  git add inst/doc
+fi
+# check if readme was update
+if [[ $CHANGED_FILES =~ .*README\.Rmd.* ]]
+then
+  R -e 'devtools::install_github("rstudio/rmarkdown"); install.packages("knitr", repos = "http://cran.rstudio.com"); knitr::knit("README.Rmd", "README.md")'
+  git add README.md
+fi
+git commit -m "Update by travis after $TRAVIS_COMMIT"
+git push origin master
